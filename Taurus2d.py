3bd9c0b3944c41ae0ae79bf0dc62beb041153093
Owen Huber <owen.huber@duke.edu>
2023-02-07 16:47:19 -0500

    some 2d functionality

--- Taurus2d.py
+++ Taurus2d.py
@@ -1,49 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from math import log
+from Taurus1d import *
 
 # Helper function working on the Taurus in 1D
 
-def Taurus_1d(boundary, x):
-    '''places x on 1D Taurus of [- boundary, + boundary]'''
-    if x < - boundary : 
-        return boundary - x % (2*boundary)
-    if x > boundary : 
-        return x % (2*boundary) - boundary
-    return x 
+def Taurus_2d(boundary, x):
+    '''places [x, y] on 2D Taurus of [- boundary, + boundary] X [- boundary, + boundary]'''
+    return [Taurus_1d(boundary, x[0]), Taurus_1d(boundary, x[1])]
 
 
 # For Simulating the heat equation on 1D Taurus
 
-def x_T1(T, dt, nu, x0, bound):
+def x_T2(T, dt, nu, x0, bound):
     '''for a single particle x0, with nu in taurus defined by the bound, finds position of x0 at time T with timesteps deltaT'''
     xn = x0
     for i in range(int(T/ dt)):
         # before converting to taurus (each step)
-        not_Taurus = np.sqrt(2*nu*dt)*np.random.normal(0,1)
-        xn += Taurus_1d(bound, not_Taurus)
-        xn = Taurus_1d(bound, xn)
+        not_Taurus1 = np.sqrt(2*nu*dt)*np.random.normal(0,1)
+        not_Taurus2 = np.sqrt(2*nu*dt)*np.random.normal(0,1)
+        temp = Taurus_2d(bound, [not_Taurus1, not_Taurus2])
+        xn[0] += temp[0]
+        xn[1] += temp[1]
+        xn = Taurus_2d(bound, xn)
     return xn
 
-def RK2_T1(T, dt, x_p, x0, h):
-    '''Using RK2 for approximations, in progress'''
-    current = x0
-    tracking = [x0]
-    for i in range(0, int(T/dt)):
-        
-        F1 = dt*x_p(current, dt*i)
-        F2 = dt*x_p(current + F1/2, dt*i + dt/2)
-        
-        current += F2 
-        tracking.append(current)
-    return tracking
 
-def u_xt1(T, dt, nu, dirac, bound):
+def u_xt2(T, dt, nu, dirac, bound):
     '''probability distribution of u given initial distribution dirac at time T'''
     record = []
     for val in dirac:
         x0 = val
-        record.append(x_T1(T, dt, nu, x0, bound))
+        record.append(x_T2(T, dt, nu, x0, bound))
     return record
 
 
@@ -68,12 +57,14 @@ def prob(a, b, lis) :
 
 # Functions for plotting in 1D
 
-def plot_1d(arr, val, **kwargs):
+def plot_2d(arr):
     '''plotting list arr around val'''
-    plt.plot(arr, np.zeros_like(arr) + val, '.', **kwargs)
+    zip(*arr)
+    plt.scatter(*zip(*arr), s = 1/4)
     plt.show()
 
-def hist1d(lis, binSize):
+
+def hist2d(lis, binSize):
     bins = np.arange(-1, 1, binSize) # fixed bin size
     plt.xlim([min(lis)-1, max(lis)+1])
     plt.hist(lis, bins=bins, alpha=0.5)
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 from Taurus1d import * 
+from Taurus2d import *
+from math import log
+import random
 
 
-
+##########################################################################################################################################
 # HEAT EQUATION ON 1D TAURUS
 
 # simulating different initial distributions
@@ -14,12 +17,32 @@ for i in range(10000):
 
 
 # our list of positions at T = 100 with time steps of 1, on the Taurus bounded by [-1, 1] 
-vals_1d = u_xt(100, 1, .01, dirac1, 1)
+vals_1d = u_xt1(100, 1, .01, dirac1, 1)
 
 
 # plotting our findings in 1D
+
 hist1d(vals_1d, 0.005)
-# plot_1d(vals_1d, 0)
+plot_1d(vals_1d, 0)
 
 # For our example list, percentage of trajectories ending up in x>0
+
 print(prob(0, 1, vals_1d))
+
+
+#########################################################################################################################################
+# HEAT EQUATION ON 2D TAURUS
+
+# simulating different initial distributions
+dirac2 = []
+for i in range(10000):
+    dirac2.append([(i%10)/2000, random.randint(1, 10)/ 2000])
+
+
+# our list of positions at T = 100 with time steps of 1, on the Taurus bounded by [-1, 1] X [-1, 1]
+vals_2d = u_xt2(100, 1, 0.00001, dirac2, 1)
+
+
+# Plotting our findings in 2D
+plot_2d(vals_2d)
+
