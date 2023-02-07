fe3a18f00c2e360647d8a4c9e39f2799a052db4a
Owen Huber <owen.huber@duke.edu>
2023-02-07 15:56:31 -0500

    adding pseudo-integration, tidying

---
+++ Taurus1d.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+# Helper function working on the Taurus in 1D
+
+def Taurus_1d(boundary, x):
+    '''places x on 1D Taurus of [- boundary, + boundary]'''
+    if x < - boundary : 
+        return boundary - x % (2*boundary)
+    if x > boundary : 
+        return x % (2*boundary) - boundary
+    return x 
+
+
+# For Simulating the heat equation on 1D Taurus
+
+def x_T1(T, dt, nu, x0, bound):
+    '''for a single particle x0, with nu in taurus defined by the bound, finds position of x0 at time T with timesteps deltaT'''
+    xn = x0
+    for i in range(int(T/ dt)):
+        # before converting to taurus (each step)
+        not_Taurus = np.sqrt(2*nu*dt)*np.random.normal(0,1)
+        xn += Taurus_1d(bound, not_Taurus)
+        xn = Taurus_1d(bound, xn)
+    return xn
+
+def u_xt(T, dt, nu, dirac, bound):
+    '''probability distribution of u given initial distribution dirac at time T'''
+    record = []
+    for val in dirac:
+        x0 = val
+        record.append(x_T1(T, dt, nu, x0, bound))
+    return record
+
+
+# for integrating
+
+def prob(a, b, lis) : 
+    '''approximate of probability that a random value of the list 'lis' is between a and b'''
+    c = min(a, b)
+    d = max(a, b)
+    i = 0
+    lis.sort()
+    # the count we wish to return
+    ret = 0
+    s = len(lis)
+    while i < s and a > lis[i] : 
+        i+= 1
+    while i < s and lis[i] <= b:
+        i += 1
+        ret += 1
+    return ret/s
+
+
+# Functions for plotting in 1D
+
+def plot_1d(arr, val, **kwargs):
+    '''plotting list arr around val'''
+    plt.plot(arr, np.zeros_like(arr) + val, '.', **kwargs)
+    plt.show()
+
+def hist1d(lis, binSize):
+    bins = np.arange(-1, 1, binSize) # fixed bin size
+    plt.xlim([min(lis)-1, max(lis)+1])
+    plt.hist(lis, bins=bins, alpha=0.5)
+    plt.title('dostribution after T')
+    plt.xlabel('x')
+    plt.ylabel('probability * 100')
+    plt.show()
--- main.py
+++ main.py
@@ -1,46 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-def Taurus_1d(boundary, x):
-    '''places x on 1D Taurus of [- boundary, + boundary]'''
-    if x < - boundary : 
-        return boundary - x % (2*boundary)
-    if x > boundary : 
-        return x % (2*boundary) - boundary
-    return x 
-
-def x_T(T, deltaT, nu, x0, bound):
-    '''for a single particle x0, with nu in taurus defined by the bound, finds position of x0 at time T with timesteps deltaT'''
-    xn = x0
-    for i in range(int(T/ deltaT)):
-        # before converting to taurus (each step)
-        not_Taurus = np.sqrt(2*nu*deltaT)*np.random.normal(0,1)
-        xn += Taurus_1d(bound, not_Taurus)
-        xn = Taurus_1d(bound, xn)
-    return xn
-
-
-def u_xt(T, deltaT, nu, dirac, bound):
-    '''probability distribution of u given initial distribution dirac at time T'''
-    record = []
-    for val in dirac:
-        x0 = val
-        record.append(x_T(T, deltaT, nu, x0, bound))
-    return record
-
-
-def plot_at_y(arr, val, **kwargs):
-    '''plotting list arr around val'''
-    plt.plot(arr, np.zeros_like(arr) + val, '.', **kwargs)
-    plt.show()
+import math
+from Taurus1d import * 
 
 
 
+# HEAT EQUATION ON 1D TAURUS
+
+# simulating different initial distributions
 dirac1 = []
+for i in range(10000):
+    dirac1.append((i%10)/20000)
 
-for i in range(100):
-    dirac1.append((i%10)/200)
 
-vals = u_xt(100, 1, .01, dirac1, 1)
+# our list of positions at T = 100 with time steps of 1, on the Taurus bounded by [-1, 1] 
+vals_1d = u_xt(100, 1, .01, dirac1, 1)
 
-plot_at_y(vals, 0)
+
+# plotting our findings in 1D
+hist1d(vals_1d, 0.005)
+# plot_1d(vals_1d, 0)
+
+# For our example list, percentage of trajectories ending up in x>0
+print(prob(0, 1, vals_1d))
